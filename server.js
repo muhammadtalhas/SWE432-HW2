29fc6a68b4e913014f61031b56287217a8558fa2
Talha Muhammad <muhammadtalhas@yahoo.com>
2017-09-19 03:42:34 -0400

    Literally idek

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ buildURI = (startDate, endDate = 0) => {
 
 // A single astroid data
 class nearEarthObject {
-    constructor(name, magnitude, diameter_feet, hazard, date, velocity, distance_miles, orbit) {
+    constructor(name, magnitude = 0, diameter_feet = -1, hazard, date, velocity, distance_miles, orbit = null) {
         this.name = name;
         this.magnitude = magnitude;
         this.diameter = diameter_feet;
@@ -43,7 +43,6 @@ class day {
     }
 }
 
-// Express setup TODO may remove if body-parser is not needed
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
@@ -51,21 +50,18 @@ app.use(bodyParser.json());
 let port = process.env.PORT || 8080;
 
 // GET returns data on a specifc date. Will be looked up once then added to cache for future requests
-app.get('/getAsteroidsByDate/:date', (req, res) => {
+app.get('/date/:date', (req, res) => {
     for (let date of cachedData) {
         if (req.params.date === date.date) {
-            console.log("found cached")
             res.send(date);
             return; // return stops further execution
         }
     }
-    console.log("not cached, looking it up")
     fetch(buildURI(req.params.date)).then(function (response) {
         return response.json();
-    }).then(function(response){
+    }).then(function (response) {
         let object = response["near_earth_objects"][req.params.date];
         let newDate = new day(req.params.date);
-        // console.log(object)
         for (let objs of object) {
             let dia = objs.estimated_diameter;
             let close = objs.close_approach_data[0];
@@ -73,11 +69,379 @@ app.get('/getAsteroidsByDate/:date', (req, res) => {
                 close.close_approach_date, close.relative_velocity, close.miss_distance.miles, close.orbiting_body);
             newDate.addNeo(NEO);
         }
-
         cachedData.push(newDate);
         res.send(newDate)
     })
-});
+})
+
+app.get('/asteroid/fastest/:date', (req, res) => {
+    for (let date of cachedData) {
+        if (req.params.date === date.date) {
+            let responseNEO = null
+            date.nearearthobjectsarray.map((NEO) => {
+                if (responseNEO === null) {
+                    responseNEO = NEO;
+                } else {
+                    if (NEO.velocity > responseNEO.velocity) {
+                        responseNEO = NEO;
+                    }
+                }
+            });
+            res.send(responseNEO);
+            return; // return stops further execution
+        }
+    }
+    fetch(buildURI(req.params.date)).then(function (response) {
+        return response.json();
+    }).then(function (response) {
+        let object = response["near_earth_objects"][req.params.date];
+        let newDate = new day(req.params.date);
+        for (let objs of object) {
+            let dia = objs.estimated_diameter;
+            let close = objs.close_approach_data[0];
+            let NEO = new nearEarthObject(objs.name, objs.absolute_magnitude_h, dia.feet, objs.is_potentially_hazardous_asteroid,
+                close.close_approach_date, close.relative_velocity, close.miss_distance.miles, close.orbiting_body);
+            newDate.addNeo(NEO);
+        }
+        cachedData.push(newDate);
+
+        let responseNEO = null
+        newDate.nearearthobjectsarray.map((NEO) => {
+            if (responseNEO === null) {
+                responseNEO = NEO;
+            } else {
+                if (NEO.velocity > responseNEO.velocity) {
+                    responseNEO = NEO;
+                }
+            }
+        });
+        res.send(responseNEO)
+    })
+})
+
+app.get('/asteroid/hazard/:date', (req, res) => {
+    for (let date of cachedData) {
+        if (req.params.date === date.date) {
+            let responseArr = []
+            date.nearearthobjectsarray.map((NEO) => {
+                if (NEO.hazard) {
+                    responseArr.push(NEO)
+                }
+            });
+            if (responseArr.length !== 0) {
+                res.send(responseArr);
+            } else {
+                res.send({"error": "No Hazards!"})
+            }
+            return; // return stops further execution
+        }
+    }
+    fetch(buildURI(req.params.date)).then(function (response) {
+        return response.json();
+    }).then(function (response) {
+        let object = response["near_earth_objects"][req.params.date];
+        let newDate = new day(req.params.date);
+        for (let objs of object) {
+            let dia = objs.estimated_diameter;
+            let close = objs.close_approach_data[0];
+            let NEO = new nearEarthObject(objs.name, objs.absolute_magnitude_h, dia.feet, objs.is_potentially_hazardous_asteroid,
+                close.close_approach_date, close.relative_velocity, close.miss_distance.miles, close.orbiting_body);
+            newDate.addNeo(NEO);
+        }
+        cachedData.push(newDate);
+
+        let responseArr = []
+        newDate.nearearthobjectsarray.map((NEO) => {
+            if (NEO.hazard) {
+                responseArr.push(NEO)
+            }
+        });
+        if (responseArr.length !== 0) {
+            res.send(responseArr);
+        } else {
+            res.send({"error": "No Hazards!"})
+        }
+    })
+})
+
+app.get('/asteroid/averageSpeed/:date', (req, res) => {
+    for (let date of cachedData) {
+        if (req.params.date === date.date) {
+            let avgMPH = 0;
+            let avgKPH = 0;
+            let avgKPS = 0;
+            let count = 0;
+            date.nearearthobjectsarray.map((NEO) => {
+                avgMPH += parseFloat(NEO.velocity.miles_per_hour);
+                avgKPH += parseFloat(NEO.velocity.kilometers_per_hour);
+                avgKPS += parseFloat(NEO.velocity.kilometers_per_second);
+                count++
+            });
+
+            avgMPH /= count
+            avgKPH /= count
+            avgKPS /= count
+            res.send({
+                "date": req.params.date,
+                "avgMPH": avgMPH,
+                "avgKPH": avgKPH,
+                "avgKPS": avgKPS
+            })
+            return; // return stops further execution
+        }
+    }
+    fetch(buildURI(req.params.date)).then(function (response) {
+        return response.json();
+    }).then(function (response) {
+        let object = response["near_earth_objects"][req.params.date];
+        let newDate = new day(req.params.date);
+        for (let objs of object) {
+            let dia = objs.estimated_diameter;
+            let close = objs.close_approach_data[0];
+            let NEO = new nearEarthObject(objs.name, objs.absolute_magnitude_h, dia.feet, objs.is_potentially_hazardous_asteroid,
+                close.close_approach_date, close.relative_velocity, close.miss_distance.miles, close.orbiting_body);
+            newDate.addNeo(NEO);
+        }
+        cachedData.push(newDate);
+
+        let avgMPH = 0;
+        let avgKPH = 0;
+        let avgKPS = 0;
+        let count = 0;
+        newDate.nearearthobjectsarray.map((NEO) => {
+            avgMPH += parseFloat(NEO.velocity.miles_per_hour);
+            avgKPH += parseFloat(NEO.velocity.kilometers_per_hour);
+            avgKPS += parseFloat(NEO.velocity.kilometers_per_second);
+            count++;
+        });
+
+        avgMPH = avgMPH / count;
+        avgKPH = avgKPH / count;
+        avgKPS = avgKPS / count;
+        res.send({
+            "date": req.params.date,
+            "avgMPH": avgMPH,
+            "avgKPH": avgKPH,
+            "avgKPS": avgKPS
+        })
+    })
+})
+
+app.get('/asteroid/averageDistance/:date', (req, res) => {
+    for (let date of cachedData) {
+        if (req.params.date === date.date) {
+            let avgDis = 0;
+            let count = 0;
+            date.nearearthobjectsarray.map((NEO) => {
+                avgDis += parseFloat(NEO.distance);
+                count++
+            });
+
+            avgDis /= count
+            res.send({
+                "date": req.params.date,
+                "avgDis_miles": avgDis,
+            })
+            return; // return stops further execution
+        }
+    }
+    fetch(buildURI(req.params.date)).then(function (response) {
+        return response.json();
+    }).then(function (response) {
+        let object = response["near_earth_objects"][req.params.date];
+        let newDate = new day(req.params.date);
+        for (let objs of object) {
+            let dia = objs.estimated_diameter;
+            let close = objs.close_approach_data[0];
+            let NEO = new nearEarthObject(objs.name, objs.absolute_magnitude_h, dia.feet, objs.is_potentially_hazardous_asteroid,
+                close.close_approach_date, close.relative_velocity, close.miss_distance.miles, close.orbiting_body);
+            newDate.addNeo(NEO);
+        }
+        cachedData.push(newDate);
+
+        let avgDis = 0;
+        let count = 0;
+        newDate.nearearthobjectsarray.map((NEO) => {
+            avgDis += parseFloat(NEO.distance);
+            count++
+        });
+
+        avgDis /= count
+        res.send({
+            "date": req.params.date,
+            "avgDis_miles": avgDis,
+        })
+    })
+})
+
+app.post('/asteroid/addNew/:date', (req, res) => {
+        if (req.params.date &&
+            req.body.name &&
+            req.body.date &&
+            req.body.hazard !== null &&
+            req.body.distance &&
+            req.body.velocity.miles_per_hour &&
+            req.body.velocity.kilometers_per_hour &&
+            req.body.velocity.kilometers_per_second) {
+
+            for (let date of cachedData) {
+                if (req.params.date === date.date) {
+
+                    for (let obj of date.nearearthobjectsarray) {
+                        if (obj.name === req.body.name) {
+                            res.sendStatus(400)
+                            return;
+                        }
+                    }
+
+                    let newName = req.body.name;
+                    let newMagnitude = (req.body.magnitude) ? req.body.magnitude : null;
+                    let newDiameter = (req.body.diameter) ? req.body.diameter : null;
+                    let newHazard = req.body.hazard;
+                    let newDateVal = req.params.date;
+                    let newVelocity = req.body.velocity;
+                    let newDistance = req.body.distance;
+                    let newOrbit = (req.body.orbit) ? req.body.orbit : null;
+
+                    let newNeo = new nearEarthObject(newName, newMagnitude, newDiameter, newHazard, newDateVal, newVelocity, newDistance, newOrbit)
+
+                    date.nearearthobjectsarray.push(newNeo);
+                    res.sendStatus(200);
+                    return;
+                }
+            }
+            fetch(buildURI(req.params.date)).then(function (response) {
+                return response.json();
+            }).then(function (response) {
+                let object = response["near_earth_objects"][req.params.date];
+                let newDate = new day(req.params.date);
+                for (let objs of object) {
+                    let dia = objs.estimated_diameter;
+                    let close = objs.close_approach_data[0];
+                    let NEO = new nearEarthObject(objs.name, objs.absolute_magnitude_h, dia.feet, objs.is_potentially_hazardous_asteroid,
+                        close.close_approach_date, close.relative_velocity, close.miss_distance.miles, close.orbiting_body);
+                    newDate.addNeo(NEO);
+                }
+                let newName = req.body.name;
+                let newMagnitude = (req.body.magnitude) ? req.body.magnitude : null;
+                let newDiameter = (req.body.diameter) ? req.body.diameter : null;
+                let newHazard = req.body.hazard;
+                let newDateVal = req.params.date;
+                let newVelocity = req.body.velocity;
+                let newDistance = req.body.distance;
+                let newOrbit = (req.body.orbit) ? req.body.orbit : null;
+
+                let newNeo = new nearEarthObject(newName, newMagnitude, newDiameter, newHazard, newDateVal, newVelocity, newDistance, newOrbit)
+
+                for (let obj of newDate.nearearthobjectsarray) {
+                    if (obj.name === req.body.name) {
+                        cachedData.push(newDate);
+                        res.sendStatus(400)
+                        return;
+                    }
+                }
+                newDate.nearearthobjectsarray.push(newNeo);
+                cachedData.push(newDate);
+                res.sendStatus(200)
+                return;
+            })
+
+
+        } else {
+            res.sendStatus(400)
+        }
+    }
+)
+
+app.put('/asteroid/update/:date', (req, res) => {
+    if (req.params.date &&
+        req.body.name &&
+        req.body.date &&
+        req.body.hazard !== null &&
+        req.body.distance &&
+        req.body.velocity.miles_per_hour &&
+        req.body.velocity.kilometers_per_hour &&
+        req.body.velocity.kilometers_per_second) {
+
+        for (let date of cachedData) {
+            if (req.params.date === date.date) {
+                for (let index in date.nearearthobjectsarray) {
+                    if (date.nearearthobjectsarray[index].name === req.body.name) {
+
+                        let newName = req.body.name;
+                        let newMagnitude = (req.body.magnitude) ? req.body.magnitude : null;
+                        let newDiameter = (req.body.diameter) ? req.body.diameter : null;
+                        let newHazard = req.body.hazard;
+                        let newDateVal = req.params.date;
+                        let newVelocity = req.body.velocity;
+                        let newDistance = req.body.distance;
+                        let newOrbit = (req.body.orbit) ? req.body.orbit : null;
+
+                        let newNeo = new nearEarthObject(newName, newMagnitude, newDiameter, newHazard, newDateVal, newVelocity, newDistance, newOrbit)
+
+                        date.nearearthobjectsarray.splice(index, 1);
+                        date.nearearthobjectsarray.push(newNeo);
+                        res.sendStatus(200)
+                        return;
+
+                    }
+                }
+                res.sendStatus(400)
+                return
+            }
+        }
+        fetch(buildURI(req.params.date)).then(function (response) {
+            return response.json();
+        }).then(function (response) {
+            let object = response["near_earth_objects"][req.params.date];
+            let newDate = new day(req.params.date);
+            for (let objs of object) {
+                let dia = objs.estimated_diameter;
+                let close = objs.close_approach_data[0];
+                let NEO = new nearEarthObject(objs.name, objs.absolute_magnitude_h, dia.feet, objs.is_potentially_hazardous_asteroid,
+                    close.close_approach_date, close.relative_velocity, close.miss_distance.miles, close.orbiting_body);
+                newDate.addNeo(NEO);
+            }
+            let newName = req.body.name;
+            let newMagnitude = (req.body.magnitude) ? req.body.magnitude : null;
+            let newDiameter = (req.body.diameter) ? req.body.diameter : null;
+            let newHazard = req.body.hazard;
+            let newDateVal = req.params.date;
+            let newVelocity = req.body.velocity;
+            let newDistance = req.body.distance;
+            let newOrbit = (req.body.orbit) ? req.body.orbit : null;
+
+            let newNeo = new nearEarthObject(newName, newMagnitude, newDiameter, newHazard, newDateVal, newVelocity, newDistance, newOrbit)
+
+            for (let index in newDate.nearearthobjectsarray) {
+                if (newDate.nearearthobjectsarray[index].name === req.body.name) {
+                    newDate.nearearthobjectsarray.splice(index, 1);
+                    newDate.nearearthobjectsarray.push(newNeo)
+                    cachedData.push(newDate);
+                    res.sendStatus(200)
+                    return;
+                }
+            }
+            cachedData.push(newDate);
+            res.sendStatus(400)
+            return;
+        })
+    } else {
+        res.sendStatus(400)
+    }
+})
+
+
+app.delete('/date/delete/:date', (req, res) => {
+    for (let index in cachedData) {
+        if (cachedData[index].date === req.params.date){
+            cachedData.splice(index, 1);
+            res.sendStatus(200)
+            return;
+        }
+    }
+    res.sendStatus(400);
+})
 
 app.listen(port, function () {
     console.log("( ͡° ͜ʖ ͡°) Hi! Im Mr. Lenny. Visit me on " + port)
